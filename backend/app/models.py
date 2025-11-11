4bd536380581cc17d5b965a82062f858d43c545b
SakibSibly <sibly@duck.com>
2025-11-11 09:30:46 +0600

    ⏬ Contacts in user deprecated and reduced to phone only

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,8 +11,18 @@ class UserBase(SQLModel):
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
     hashed_password: str = Field(default=None, max_length=256)
-    security_qas: list["SecurityQA"] = Relationship(back_populates="user", cascade_delete=True)
-    contacts: list["Contact"] = Relationship(back_populates="user", cascade_delete=True)
+    security_qas: list["SecurityQA"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"}, cascade_delete=True)
+    phones: list["Phone"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"}, cascade_delete=True)
+
+
+class UserPublic(UserBase):
+    id: uuid.UUID
+
+
+class UserBaseWithPhoneAndQuestion(UserBase):
+    id: uuid.UUID
+    phones: list["Phone"] = []
+    security_qas: list["SecurityQA"] = []
 
 
 class UserLogin(SQLModel):
@@ -32,40 +42,32 @@ class SecurityQABase(SQLModel):
 
 class SecurityQA(SecurityQABase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
-    user: User | None = Relationship(back_populates="security_qas")
+    user: User | None = Relationship(back_populates="security_qas", sa_relationship_kwargs={"lazy": "joined"})
+
+
+class SecurityQAWithUser(SecurityQABase):
+    id: uuid.UUID
+    user: UserPublic | None = None
 
 
 class SecrurityQACreate(SecurityQABase):
     pass
 
 
-class ContactBase(SQLModel):
-    name: str = Field(default=None, index=True, max_length=100)
-    email: EmailStr | None = Field(default=None, index=True, max_length=100)
-    address: str | None = Field(default=None, max_length=255)
-    notes: str | None = Field(default=None, max_length=500)
-    user_id: uuid.UUID | None = Field(foreign_key="user.id", ondelete="CASCADE")
-
-
-class Contact(ContactBase, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
-    phones: list["Phone"] = Relationship(back_populates="phone_contacts", cascade_delete=True)
-    user: User | None = Relationship(back_populates="contacts")
-
-
-class ContactCreate(ContactBase):
-    pass
-
-
 class PhoneBase(SQLModel):
     number: str = Field(default=None, max_length=20)
     number_type: str | None = Field(default=None, max_length=50)
-    contacts_id: uuid.UUID | None = Field(foreign_key="contact.id", ondelete="CASCADE")
+    user_id: uuid.UUID | None = Field(default=None, foreign_key="user.id", ondelete="CASCADE")
 
 
 class Phone(PhoneBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
-    phone_contacts: Contact | None = Relationship(back_populates="phones")
+    user: User | None = Relationship(back_populates="phones", sa_relationship_kwargs={"lazy": "joined"})
+
+
+class PhoneWithUser(PhoneBase):
+    id: uuid.UUID
+    user: UserPublic | None = None
 
 
 class PhoneCreate(PhoneBase):
